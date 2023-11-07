cedb4d6f9f10d806ee77eafb4f51822d65da6c79
Masaruma <146043190+Masaruma@users.noreply.github.com>
2023-11-08 01:14:13 +0900

    Add files via upload

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 'use strict'
 // 1行目に記載している 'use strict' は削除しないでください
-
+// domを取得
 const container = document.querySelector(".container");
 const text = document.getElementById("text");
 
-
-let totalTime = 21000;
-let breathTime = 8000;
+// 息を数時間を設定
+let totalTime = 20000;
+let breathTime = 7000;
 let holdTime = 0;
 
 
 
-
+// テキストを切り替える関数
 function zazenAnimation() {
-  text.innerHTML = "息を吸います";
+  text.innerText = "息を吸います";
   container.className = "container grow";
   setTimeout(() => {
-    text.innerHTML = "息を吐きます";
+    text.innerText = "息を吐きます";
     container.className = "container shrink";
     // setTimeout(() => {
     //   text.innerHTML = "吐きましょう";
@@ -29,3 +29,7 @@ function zazenAnimation() {
 
 zazenAnimation();
 setInterval(zazenAnimation, totalTime);
+
+// ボタンを押し10分経過するとポップアップ
+const button = document.getElementById("button")
+button.addEventListener("click", () => setTimeout(() => alert("10分経過しました。"), 600 * 1000));
